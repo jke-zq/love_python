7bf20d5126a2dfe107126a5de5a768e0dbee9f2e
jke-zq <jke0zq@gmail.com>
2016-06-16 22:39:52 +0800

    decorator and utf8

--- decorator.py
+++ decorator.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*- 
+
 import functools
+#################################using function as decorator#####################
 def simpleDecorator(f):
     @functools.wraps(f)
     def _wrapper(*args, **kw):
@@ -45,6 +48,89 @@ def commDecorator(*args):
 def foo():
     for __ in range(1000000):
         pass
+
+######################################using class as decorator#############################
+class DecoratorClass(object):
+ 
+    def __init__(self, fn):
+        self.fn = fn
+ 
+    def __call__(self):
+        self.fn()
+ 
+class DecoratorClassWithArgs(object):
+
+    def __init__(self, args):
+        self.args = args
+
+    def __call__(self, func):
+        def _wrapper(*args, **kw):
+            print self.args
+            func(*args, **kw)
+        return _wrapper
+         
+
+@DecoratorClassWithArgs('args')
+# @DecoratorClass
+def fooFun():
+    print 'fooFun with decorator class'
+fooFun()
+
+####################################decorate a class###################################
+import inspect
+
+def log(func):
+    def wrapped(*args, **kwargs):
+        try:
+            print "Entering: [%s] with parameters %s" % (func.__name__, args)
+            try:
+                return func(*args, **kwargs)
+            except Exception, e:
+                print 'Exception in %s : %s' % (func.__name__, e)
+        finally:
+            print "Exiting: [%s]" % func.__name__
+    return wrapped
+
+def trace(cls):
+    for name, m in inspect.getmembers(cls, inspect.ismethod):
+        setattr(cls,name,log(m))
+    return cls
+
+#@trace(log)
+# class trace(object):
+
+#     def __init__(self, f):
+#         self.f = f
+
+#     def __call__(self, cls):
+#         for name, m in inspect.getmembers(cls, inspect.ismethod):
+#             setattr(cls, name, self.f(m))
+#         return cls
+
+
+@trace
+class X(object):
+
+    def first_x_method(self):
+        print 'doing first_x_method stuff...'
+
+    def second_x_method(self):
+        print 'doing second_x_method stuff...'
+################################some notes########################
+## more @ 
+# @decorator_one
+# @decorator_two
+# def func():
+#     pass
+# 相当于：
+# func = decorator_one(decorator_two(func))
+
+## more args
+# @decorator(arg1, arg2)
+# def func():
+#     pass
+# 相当于：
+# func = decorator(arg1,arg2)(func)
 
 if __name__ == '__main__':
     foo()
