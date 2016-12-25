c766c453dcf6aaf65748e6921f14ebefa48451a8
jke-zq <jke0zq@gmail.com>
2016-12-26 00:09:05 +0800

    inherit

---
+++ inherit.py
@@ -0,0 +1,11 @@
+# Demo1:__url
+# cant get __url from parent class
+
+# Demo2:get method when inheriting from more parent
+# find from left to right
+
+# Demo3:__init__ acts like normal methods, is not the constructing method
+# You should use super().__init__ to call parent's __init__ method in __init__
+
+# loop importing module
+# import the module just above the called line instead of the beginning
