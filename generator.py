a1e8af9723e7cf51abfeda3abdba4b6fd906f486
jke-zq <jke0zq@gmail.com>
2016-12-26 00:08:53 +0800

    generator

---
+++ generator.py
@@ -0,0 +1,6 @@
+# yield in recursion
+# 'for a in generator_func: yield a' or 'yield from generator_fun'
+
+# control to read a line from a file
+# yield in for in open('file') and then next the generator
+
